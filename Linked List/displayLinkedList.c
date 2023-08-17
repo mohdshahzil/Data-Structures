c16e394aa715ca42eb1b12bd8ffd723041360196
mohdshahzil <theshahzil@gmail.com>
2023-08-18 02:28:41 +0530

    displaying linked list and creation

---
+++ Linked List/displayLinkedList.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d\n", p->data);
+        p = p->next;
+    }
+}
+
+int main()
+{
+    int A[] = {3, 5, 7, 10, 15};
+    create(A, 5);
+    Display(first);
+
+    return 0;
+}
