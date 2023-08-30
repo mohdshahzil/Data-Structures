7c715fa0e8469c02e7da74a7b57572bdb6bd7e92
mohdshahzil <theshahzil@gmail.com>
2023-08-30 23:58:43 +0530

    count-and-sum

---
+++ Linked List/countingNodes.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+int count(struct Node *p)
+{
+    int c = 0;
+    while (p != NULL)
+    {
+        c++;
+        p = p->next;
+    }
+    return c;
+}
+
+int main()
+{
+    int A[] = {3, 5, 7, 10, 15};
+    create(A, 5);
+    printf("%d", count(first));
+
+    return 0;
+}
