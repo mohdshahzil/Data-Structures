8f5d976bf26a9ec61788c6a55ad28f740fc4dc89
mohdshahzil <theshahzil@gmail.com>
2023-09-01 00:59:01 +0530

    inserting

---
+++ Linked List/insertingInLL.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+} *first = NULL;
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+int count(struct Node *p)
+{
+    int c = 0;
+    while (p != NULL)
+    {
+        c++;
+        p = p->next;
+    }
+    return c;
+}
+
+void Insert(struct Node *p, int index, int x)
+{
+    if (index < 0 || index > count(p))
+    {
+        return;
+    }
+
+    struct Node *t;
+    t = (struct Node *)malloc(sizeof(struct Node));
+    t->data = x;
+
+    if (index == 0)
+    {
+        t->next = first;
+        first = t;
+    }
+    else
+    {
+        for (int i = 0; i < index - 1; i++)
+        {
+            p = p->next;
+        }
+        t->next = p->next;
+        p->next = t;
+    }
+}
+
+int main()
+{
+    int A[] = {10, 20, 30, 40, 50};
+    create(A, 5);
+
+    Insert(first, 0, 5);
+
+    Display(first);
+
+    return 0;
+}
